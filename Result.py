e290a11ece628b1d560569043adfcf73d8757b70
Jiabao (Chloe) Zeng <chloe_52_@hotmail.com>
2017-11-27 23:00:42 -0500

    Add files via upload

--- Result.py
+++ Result.py
@@ -6,38 +6,27 @@ Created on Sat Nov  4 20:57:20 2017
 @author: chloezeng
 """
 
-from nltk.classify.util import accuracy
-from nltk.metrics.scores import precision as precision
-from nltk.metrics.scores import recall as recall
-import collections
+from sklearn.metrics import classification_report, accuracy_score
 
 class Result:
     def __init__(self, train, val, clf):
         self.trainFeatures = train
         self.valFeatures = val
         
-        #initiates trueSets and predSets
-        trueSets = collections.defaultdict(set)
-        predSets = collections.defaultdict(set)
+        #initiates global_label and global_result
+        self.global_label = []
+        self.global_result = []
         
         #puts correctly labeled sentences in trueSets and the predictively labeled version in predSets
         for i, (features, label) in enumerate(self.valFeatures):
-            trueSets[label].add(i)
-            predicted = clf.classify(features)
-            predSets[predicted].add(i)
-            
-        self.accuracy = accuracy(clf, self.valFeatures)
-        self.pos_precision = precision(trueSets['pos'], predSets['pos'])
-        self.pos_recall = recall(trueSets['pos'], predSets['pos'])
-        self.neg_precision = precision(trueSets['neg'], predSets['neg'])
-        self.neg_recall = recall(trueSets['neg'], predSets['neg'])
+            self.global_label.append(label)
+            self.global_result.append(clf.classify(features))
+
         
     def print_result(self):
-        print 'train on %d instances, test on %d instances' % (len(self.trainFeatures), len(self.valFeatures))
-        print 'accuracy:', self.accuracy
-        print 'pos precision:', self.pos_precision
-        print 'pos recall:', self.pos_recall
-        print 'neg precision:', self.neg_precision
-        print 'neg recall:', self.neg_recall
+        print('Accuracy: {}'.format(accuracy_score(self.global_label, self.global_result)))
+        print classification_report(self.global_label, self.global_result)
+        
+
         
         
